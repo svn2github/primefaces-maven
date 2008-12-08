f60306d7c400895898d9a91f906254bc86311e8a
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2008-12-08 18:37:17 +0000

    Add resource for commons digester
    git-svn-id: http://primefaces.googlecode.com/svn/maven/trunk@166 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ maven-jsf-plugin/src/main/java/com/prime/yui4jsf/jsfplugin/digester/Resource.java
@@ -0,0 +1,20 @@
+package com.prime.yui4jsf.jsfplugin.digester;
+
+public class Resource {
+
+	private String name;
+	
+	public Resource() {}
+
+	public Resource(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
