e774eb14488af3aed6efcba332d187814610611a
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-04-01 08:28:49 +0000

    Added archetype
    git-svn-id: http://primefaces.googlecode.com/svn/maven/trunk@2550 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ primefaces-jsf20-archetype/src/main/resources/archetype-resources/src/main/java/MainView.java
@@ -0,0 +1,20 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class MainView {
+
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
