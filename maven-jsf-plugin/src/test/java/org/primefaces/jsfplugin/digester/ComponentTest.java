80538fa0636bee161e8431b6f17b5f03560ce5a4
sudheer.jonna@gmail.com <sudheer.jonna@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-09-19 03:58:43 +0000

    component paths updated
    git-svn-id: http://primefaces.googlecode.com/svn/maven/trunk@11859 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- maven-jsf-plugin/src/test/java/org/primefaces/jsfplugin/digester/ComponentTest.java
+++ maven-jsf-plugin/src/test/java/org/primefaces/jsfplugin/digester/ComponentTest.java
@@ -21,7 +21,7 @@ public class ComponentTest extends TestCase {
 
 	public void testGetShortName() {
 		Component component = new Component();
-		component.setComponentClass("net.sf.yui4jsf.component.Slider");
+		component.setComponentClass("org.primefaces.component.slider.Slider");
 		assertEquals("Slider", component.getComponentShortName());
 	}
 	
@@ -33,13 +33,13 @@ public class ComponentTest extends TestCase {
 	
 	public void testGetParentPackagePath() {
 		Component component = new Component();
-		component.setTagClass("net.sf.yui4jsf.component.tabview.Tab");
+		component.setTagClass("org.primefaces.component.tabview.TabTag");
 		assertEquals("tabview", component.getParentPackagePath());
 	}
 	
 	public void testGetPackage() {
 		Component component = new Component();
-		component.setTagClass("net.sf.yui4jsf.component.tabview.Tab");
-		assertEquals("net.sf.yui4jsf.component.tabview", component.getPackage());
+		component.setTagClass("org.primefaces.component.tabview.TabTag");
+		assertEquals("org.primefaces.component.tabview", component.getPackage());
 	}
 }
